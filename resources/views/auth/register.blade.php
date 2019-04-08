b74f6d9d5bdc355d68219990c196bc78714d20db
Marius <marius@gluocode.com>
2019-04-08 13:13:15 +0200

    Add password requirements to registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -59,6 +59,9 @@
                                           <strong>{{ $errors->first('password') }}</strong>
                                       </span>
                                   @endif
+                                  <p id="passwordHelpBlock" class="form-text text-muted">
+                                    Your password must be more than 8 characters long, should contain at-least 1 Uppercase, 1 Lowercase, 1 Numeric and 1 special character.
+                                  </p>
                               </div>
                           </div>
 
