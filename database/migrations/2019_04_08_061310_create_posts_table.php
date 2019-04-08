a7ab56e15008155696a70241027bc36f392fd07d
Marius <marius@gluocode.com>
2019-04-08 19:07:21 +0200

    Add elements to Post Table

--- database/migrations/2019_04_08_061310_create_posts_table.php
+++ database/migrations/2019_04_08_061310_create_posts_table.php
@@ -16,6 +16,8 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
+            $table->text('body');
+            $table->integer('user_id');
         });
     }
 
